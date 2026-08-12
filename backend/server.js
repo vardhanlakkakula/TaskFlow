2662c880f3a2657890de0e10a4aa939b1e528757
Lakkakula Vardhan <lakkakulavardhan647@gmail.com>
2026-08-12 23:37:44 +0530

    Configure production CORS

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,12 @@ import dashboardRoutes from "./routes/dashboardRoutes.js";
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
